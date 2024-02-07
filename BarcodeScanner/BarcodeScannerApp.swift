47cf3bde6e0c0e6c98ad4923ba4893d7ff3a2c6b
dmontalto <dommontalto@outlook.com>
2024-02-07 20:53:15 +0700

    Initial Commit

---
+++ BarcodeScanner/BarcodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeScannerApp.swift
+//  BarcodeScanner
+//
+//  Created by Dominic Montalto on 07/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct BarcodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
